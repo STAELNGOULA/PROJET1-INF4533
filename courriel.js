db93d135c2abaa14b4c3a350537c489b5f28bed8
STAELNGOULA <35429376+STAELNGOULA@users.noreply.github.com>
2018-02-27 16:04:34 -0500

    Add files via upload

---
+++ courriel.js
@@ -0,0 +1,22 @@
+
+
+
+    var element = document.getElementById('write');
+
+
+    element.onclick = function() {
+
+        alert("Voulez-vous ecrire un nouveau message");
+
+    };
+
+
+
+    var element = document.getElementById('write2');
+
+
+    element.onclick = function() {
+
+        alert(confirm("Voulez-vous supprimer un message?"));
+
+    };
